ab9bdf5ffc1171146b4151ab130936639f0ca5a1
trina77 <100989592+trina77@users.noreply.github.com>
2024-06-04 21:52:59 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './components/style.css';
+import Meal from './components/Meal';
+import Color from './components/Color';
+const App = () => {
+  return (
+    <>
+      <Meal/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Color.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Color = () => {
+  return (
+    <div className='flex justify-center items-center h-[100vh]'>
+        <button className='bg-blue-500 text-white h-16 w-auto px-3 rounded-full hover:bg-pink-500 transition-colors shadow-lg' > Search here ROUSHAN </button>
+    </div>
+    
+  )
+}
+
+export default Color
